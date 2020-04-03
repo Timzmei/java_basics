e694f61cd6b19cc488f97803753f4ef3ff06103f
Guliev Timur <timur.guliev@gmail.com>
2020-04-03 22:14:42 +0500

    ToDoList рабочая версия 03.04.2020

--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,17 +1,76 @@
+/**
+ *     URL             Method              Description
+ *
+ *     /list/          GET                 Список всех задач
+ *
+ *     [
+ *          {
+ *              "id": 23,
+ *              "name": "Заполнить БД",
+ *              "date": 01.01.2001,
+ *              "description": "БД должна содержать поля..... ",
+ *          },
+ *          {
+ *              "id": 24,
+ *              "name": "Сходить в магазин",
+ *              "date": 01.04.2020,
+ *              "description": "Купить: гречку, туалетную бумагу, дробовик, патроны",
+ *          },
+ *      ]
+ *
+ *     /list/ID        GET                 Конкретная задача
+ *
+ *     {
+ *         "id": 23,
+ *         "name": "Заполнить БД",
+ *         "date": 01.01.2001,
+ *         "description": "БД должна содержать поля..... ",
+ *     }
+ *
+ *     /list/          POST                Добавление новой задачи
+ *
+ *     {
+ *         "name": "Заполнить БД",
+ *         "date": 01.01.2001,
+ *         "description": "БД должна содержать поля..... ",
+ *     }
+ *
+ *
+ *
+ *     /list/ID        PUT                 Сохранение изменений задачи целиком
+ *
+ *     {
+ *         "name": "Заполнить БД",
+ *         "date": 01.01.2001,
+ *         "description": "БД должна содержать поля..... ",
+ *     }
+ *
+ *     /list/ID        PATCH               Сохранение изменений отдельных свойств
+ *
+ *     {
+ *         "description": "БД должна содержать поля..... ",
+ *     }
+ *
+ *     /list/ID        DELETE              Удаление задачи
+ *
+ *
+ *
+ *
+ *
+ */
+
 package main;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 
-@RestController
-
+@Controller
 public class DefaultController
 {
     @RequestMapping("/")
     public String index(){
-        return (new Date()).toString();
+        return "index";
     }
 
 }
---
+++ 12_SpringBootWebDevelopment/ToDoList/target/classes/static/js/main.js
@@ -0,0 +1,76 @@
+$(function(){
+
+    const appendBook = function(data){
+        var bookCode = '<a href="#" class"book-link" data-id="' +
+        data.id + '">' + data.name + '</a>' +
+        'Год выпуска: ' + data.year;;
+        $('#todo-list')
+            .append('<div>' + bookCode + '</div>');
+    };
+
+    //Loading books on load page
+    $.get('/list/', function(response)
+    {
+        for(i in response) {
+            appendBook(response[i]);
+        }
+    });
+
+    //Show adding book form
+    $('#show-add-book-form').click(function(){
+        $('#book-form').css('display', 'flex');
+    });
+
+    //Closing adding book form
+    $('#book-form').click(function(event){
+        if(event.target === this) {
+            $(this).css('display', 'none');
+        }
+    });
+
+    //Getting book
+    $(document).on('click', '.book-link', function(){
+        var link = $(this);
+        var bookId = link.data('id');
+        $.ajax({
+            method: "GET",
+            url: '/link/' + bookId,
+            success: function(response)
+            {
+                var code = '<span>Год выпуска:' + response.year + '</span>';
+                link.parent().append(code);
+            },
+            error: function(response)
+            {
+                if(response.status == 404) {
+                    alert('Книга не найдена!');
+                }
+            }
+        });
+        return false;
+    });
+
+    //Adding book
+    $('#save-book').click(function()
+    {
+        var data = $('#book-form form').serialize();
+        $.ajax({
+            method: "POST",
+            url: '/list/',
+            data: data,
+            success: function(response)
+            {
+                $('#book-form').css('display', 'none');
+                var book = {};
+                book.id = response;
+                var dataArray = $('#book-form form').serializeArray();
+                for(i in dataArray) {
+                    book[dataArray[i]['name']] = dataArray[i]['value'];
+                }
+                appendBook(book);
+            }
+        });
+        return false;
+    });
+
+});
