8e9c60376d977ccae09f29375f85bc93e2f455fb
Guliev Timur <timur.guliev@gmail.com>
2020-04-22 23:51:26 +0500

    ToDoList рабочая версия 22.04.2020

--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoController.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoController.java
@@ -2,46 +2,60 @@ package main;
 
 
 import main.model.ToDo;
+import main.model.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ToDoController
 {
+    @Autowired
+    private ToDoRepository toDoRepository;
+
     @GetMapping("/list/")
     public List<ToDo> list()
     {
-        return Storage.getAllToDos();
+        Iterable<ToDo> toDoIterable = toDoRepository.findAll();
+        ArrayList<ToDo> toDos = new ArrayList<>();
+        for (ToDo toDo : toDoIterable) {
+            toDos.add(toDo);
+        }
+        return toDos;
     }
 
     @PostMapping("/list/")
     public int add(ToDo toDo)
     {
-        return Storage.addToDo(toDo);
+        ToDo newToDo = toDoRepository.save(toDo);
+        return newToDo.getId();
     }
 
     @PutMapping("/list/")
     public ToDo edit(ToDo toDo)
     {
-        return Storage.editToDo(toDo);
+        toDoRepository.save(toDo);
+        return toDo;
     }
 
-    @PutMapping("/list/{id}")
+    @DeleteMapping("/list/{id}")
     public void delete(@PathVariable int id)
     {
-        Storage.deleteToDo(id);
+        toDoRepository.deleteById(id);
     }
 
     @GetMapping("/list/{id}")
     public ResponseEntity get(@PathVariable int id)
     {
-        ToDo toDo = Storage.getToDo(id);
-        if(toDo == null) {
+        Optional<ToDo> optionalToDo = toDoRepository.findById(id);
+        if(!optionalToDo.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        return new ResponseEntity(toDo, HttpStatus.OK);
+        return new ResponseEntity(optionalToDo.get(), HttpStatus.OK);
     }
 }
--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/model/ToDo.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/model/ToDo.java
@@ -1,9 +1,15 @@
 package main.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
-
+@Entity
 public class ToDo
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private int status;
